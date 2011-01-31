4d0a027d7cf7dea672611afabe0252748b0a9267
Alex Joyce <alexjoyce@ewb.ca>
2011-01-30 21:00:22 -0500

    added categories, budget etc. applications to INSTALLED_APPS

--- settings.py
+++ settings.py
@@ -1,5 +1,7 @@
 # Django settings for syde461 project.
 
+import os.path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -10,8 +12,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-#DATABASE_NAME = 'C:/Users/Alex Joyce/Documents/Waterloo/461designproject/engsoc.db'
-DATABASE_NAME = 'C:/Users/katrina/Documents/461designproject/engsoc.db'             # Or path to database file if using sqlite3.
+DATABASE_NAME = 'C:/Users/Alex Joyce/Documents/Waterloo/461designproject/engsoc.db'
+#DATABASE_NAME = 'C:/Users/katrina/Documents/461designproject/engsoc.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -72,8 +74,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    #"C:/Users/Alex Joyce/Documents/Waterloo/461designproject/templates"
-    "C:/Users/katrina/Documents/461designproject/templates"
+    "C:/Users/Alex Joyce/Documents/Waterloo/461designproject/templates"
+    #"C:/Users/katrina/Documents/461designproject/templates"
 )
 
 INSTALLED_APPS = (
@@ -83,5 +85,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    '461designproject.transactions'
+    '461designproject.transactions',
+    '461designproject.positions',
+    '461designproject.budget',
+    '461designproject.categories'
 )
