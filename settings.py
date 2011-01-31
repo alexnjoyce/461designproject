b91362a6c20fc7dd44fea12b3d26e956ccde3920
katriiina <katrina@.(none)>
2011-01-31 11:29:27 -0500

    adding settings.py for alex

--- settings.py
+++ settings.py
@@ -12,8 +12,8 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'C:/Users/Alex Joyce/Documents/Waterloo/461designproject/engsoc.db'
-#DATABASE_NAME = 'C:/Users/katrina/Documents/461designproject/engsoc.db'             # Or path to database file if using sqlite3.
+#DATABASE_NAME = 'C:/Users/Alex Joyce/Documents/Waterloo/461designproject/engsoc.db'
+DATABASE_NAME = 'C:/Users/katrina/Documents/461designproject/engsoc.db'             # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -74,8 +74,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/Users/Alex Joyce/Documents/Waterloo/461designproject/templates"
-    #"C:/Users/katrina/Documents/461designproject/templates"
+    #"C:/Users/Alex Joyce/Documents/Waterloo/461designproject/templates"
+    "C:/Users/katrina/Documents/461designproject/templates"
 )
 
 INSTALLED_APPS = (
