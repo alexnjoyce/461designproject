56c5024cafff5c01948655b3aa69adffbe1b8b55
Alex Joyce <alexjoyce@ewb.ca>
2011-02-12 20:32:31 -0500

    updated urls to bring in transaction to naming conventions

--- urls.py
+++ urls.py
@@ -18,24 +18,13 @@ urlpatterns = patterns('',
     
 )
 
-#all our url patterns go here
-urlpatterns += patterns('transactions.views',
-    
-    url(r'^$','index', name="index"),
-    url(r'^create/income/$',                'create_income',                name="create_income"),
-    url(r'^create/expenditure/$',           'create_expenditure',           name="create_expenditure"),
-    url(r'^view/$',                         'view_all',                     name="view_all"),
-    url(r'^edit/(?P<id>\d+)/$',             'edit_transaction',             name="edit_transaction"),
-    url(r'^confdelete/(?P<id>\d+)/$',       'confirm_delete_transaction',   name="confirm_delete_transaction"),
-    url(r'^delete/(?P<id>\d+)/$',           'delete_transaction',           name="delete_transaction"),
-
-)
 
 #include file by file for each of the url.py files in the app folders
 urlpatterns += patterns('',
     (r'', include('positions.urls')),
     (r'', include('budget.urls')),
     (r'', include('categories.urls')),
+    (r'', include('transactions.urls')),
     
 
 )
